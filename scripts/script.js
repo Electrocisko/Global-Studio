a1cea4a33f8e1f7a372ec9cc3e3aa134df969983
Francisco Zuidwijk <electrocisko@gmail.com>
2023-07-31 14:45:27 -0300

    primer commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+// const imagenSlider = document.getElementById("imagen-slider");
+
+// console.log(imagenSlider.src)
+
+// const imgArray = ['../imagenes/GIF_BRAND.gif'];
+
+// let i = 0;
+
+// const slideShow = () =>{
+//     console.log(`Slideshow${i}`)
+//     imagenSlider.src = imgArray[i]
+//     i = ( i<imgArray.length-1) ? i+1 : 0;
+// }
+
+// const interval = () =>{
+//     console.log("interval")
+//     setInterval(slideShow,2500)
+// }
+
+
+
+
